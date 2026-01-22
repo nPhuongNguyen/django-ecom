e8fb9463ca0c146746df84d0c83d0310a0749e18
nPhuongNguyen <phuong0909tt@gmail.com>
2026-01-22 14:04:56 +0700

    fix json dump

--- apps/logging/logging_log.py
+++ apps/logging/logging_log.py
@@ -1,10 +1,13 @@
 
+from datetime import date, datetime, time
+from decimal import Decimal
 import json
 import logging
 import sys
+from uuid import UUID
 from django.conf import settings
 from django.utils import timezone
-
+from django.db.models import Model, QuerySet
 from apps.config.kafka_config import PushO2mSmartlinkAPILog
 from apps.utils import request_func
 logger = logging.getLogger("o2m-smart-link-api-logging")
@@ -22,7 +25,29 @@ if not logger.handlers:
     ch = logging.StreamHandler()
     ch.setFormatter(JsonFormatter())
     logger.addHandler(ch)
+def safe_json(obj):
+    # JSON không hỗ trợ các kiểu dữ liệu như Decimal, datetime, UUID, QuerySet, Model
+    if isinstance(obj, Decimal): # Chuyển Decimal thành str
+        return str(obj)
 
+    if isinstance(obj, (datetime, date, time)): # Chuẩn hóa datetime, date, time thành ISO 8601
+        return obj.isoformat()
+
+    if isinstance(obj, UUID): # Chuyển UUID thành str
+        return str(obj)
+
+    if isinstance(obj, QuerySet): # Chuyển QuerySet thành list
+        return list(obj.values())
+
+    if isinstance(obj, Model): # Chuyển Model thành str (hoặc có thể sử dụng một serializer nếu cần)
+        return str(obj)
+
+    if isinstance(obj, dict): # Chuyển dict thành dict với các giá trị đã được xử lý
+        return {k: safe_json(v) for k, v in obj.items()}
+
+    if isinstance(obj, (list, tuple, set)): # Chuyển list, tuple, set thành list với các phần tử đã được xử lý
+        return [safe_json(v) for v in obj]
+    return str(obj)
 def _log(level: str, message=None, **kwargs):
     log_data = {
         "timestamp": timezone.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3],
@@ -34,7 +59,10 @@ def _log(level: str, message=None, **kwargs):
     if kwargs:
         log_data.update(kwargs)
 
-    json_msg = json.dumps(log_data, ensure_ascii=False)
+    json_msg = json.dumps(
+        safe_json(log_data),
+        ensure_ascii=False
+    )
     has_exception = sys.exc_info()[0] is not None
     try:
         if level == "ERROR":
