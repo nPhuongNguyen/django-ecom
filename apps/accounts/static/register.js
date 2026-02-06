4202373279605ea4f66f10d636bca12a5de96d51
nPhuongNguyen <phuong0909tt@gmail.com>
2026-02-06 11:50:04 +0700

    update verify otp

--- apps/accounts/static/register.js
+++ apps/accounts/static/register.js
@@ -53,27 +53,4 @@ $(document).ready(function () {
         }
     )
 
-    const confirm_form$ = $('#confirm_form');
-    const confirm_validator = FormValidateLoader.init(
-        confirm_form$,
-        {
-            submitHandler:async function (form, event) {
-                event.preventDefault();
-                const formdata = FormValidateLoader.formData(confirm_form$);
-                const result_api = await CallApi.request({
-                    url: confirm_form$.data('url'),
-                    method: 'POST',
-                    data: formdata
-                })
-                if (result_api){
-                    if (result_api.status_code === 1){
-                        modal_confirm$.hide();
-                    }
-                    else{
-                        confirm_validator.showErrors(result_api.errors)
-                    }
-                }
-            }
-        }
-    )
 })
--- apps/accounts/views/verify_otp.py
+++ apps/accounts/views/verify_otp.py
@@ -3,4 +3,7 @@ from django.views import View
 
 class VerifyOTPView(View):
     def get(self, request, *args, **kwargs):
-        return render(request,'verify-otp.html')
+        check_email = kwargs.get('email')
+        if not check_email:
+            return render(request,'admin/notfound/notfound.html')
+        return render(request,'verify-otp.html', context={'email': check_email})
