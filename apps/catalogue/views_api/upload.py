4a09da2496c65f228a26822c6aafd809d4dfa9eb
nPhuongNguyen <phuong0909tt@gmail.com>
2026-04-03 10:10:22 +0700

    add rquest.files data input

--- apps/catalogue/views_api/upload.py
+++ apps/catalogue/views_api/upload.py
@@ -1,31 +1,45 @@
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
-
+from apps.logging import logging_log as lg
 from apps.catalogue.serializers.upload import UploadImageSerializer
+from ...shared.decorator.decorator import validate_exception
 from apps.shared.response import ResponseBuilder, ResponseCodes
 from apps.utils.utils_handle_upload import handle_upload
 
 class UploadImageAPI(APIView):
     parser_classes = [MultiPartParser, FormParser]
     # @token_required()
+    @validate_exception()
     def post(self, request, *args, **kwargs):
-        data_input = request.data_input
+        data_input = request.files
         serializer = UploadImageSerializer(data=data_input)
         if not serializer.is_valid():
+            lg.log_error(
+                "[UploadImageAPI] Invalid input",
+                errors=serializer.errors
+            )
             return ResponseBuilder.build(
                 code=ResponseCodes.INVALID_INPUT,
                 errors=serializer.errors
             )
         validated_data = serializer.validated_data
-        data_body_input = validated_data.get("body", {})
-        input_img = data_body_input.get('list_image')
+        input_img = validated_data.get('list_image')
         folder = "product"
         result_img = handle_upload(input_img, folder)
         if result_img is None:
+            lg.log_error(
+                "[UploadImageAPI] Upload failed",
+                folder=folder
+            )
             return ResponseBuilder.build(
                code=ResponseCodes.SYSTEM_BUSY,
                errors="Lỗi upload"
             )
+        lg.log_info(
+            "[UploadImageAPI] Upload successful",
+            folder=folder,
+            result=result_img
+        )
         return ResponseBuilder.build(
             code=ResponseCodes.SUCCESS,
             data = {
